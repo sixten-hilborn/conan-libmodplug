8eb0135f538d79db0475fe37a0d4cfd9a483deb8
Sixten Hilborn <hilborn90@gmail.com>
2018-01-04 10:30:38 +0100

    Use bincrafters templace

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,28 @@
-from conans import ConanFile, CMake
-from conans.tools import download, unzip
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
 import os
 import shutil
+
 
 class LibmodplugConan(ConanFile):
     name = "libmodplug"
     version = "0.8.8.5"
-    description = "libmodplug plays mod-like music formats"
-    folder = "libmodplug-%s" % version
+    url = "https://github.com/sixten-hilborn/conan-libmodplug"
+    description = "Plays mod-like music formats"
+    
+    # Indicates License type of the packaged library
+    license = "Public Domain"
+    
+    # Packages the license for the conanfile.py
+    exports = ["LICENSE.md"]
+    
+    # Remove following lines if the target lib does not use cmake.
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake" 
+    
+    # Options may need to change depending on the packaged library. 
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -17,41 +32,41 @@ class LibmodplugConan(ConanFile):
         'shared=False',
         'fPIC=True'
     )
-    generators = "cmake"
-    exports = ["CMakeLists.txt"]
-    url = "https://github.com/sixten-hilborn/conan-libmodplug"
-    license = "Public Domain"
+
+    # Custom attributes for Bincrafters recipe conventions
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
 
 
     def source(self):
-        zip_name = "%s.tar.gz" % self.folder
-        download("http://sourceforge.net/projects/modplug-xmms/files/{0}/{1}/{2}/download".format(self.name, self.version, zip_name), zip_name)
-        unzip(zip_name)
-        os.unlink(zip_name)
-        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.folder)
+        extracted_dir = self.name + "-" + self.version
+        tools.get("http://sourceforge.net/projects/modplug-xmms/files/{0}/{1}/{2}.tar.gz".format(self.name, self.version, extracted_dir))
 
+        #Rename to "source_subfolder" is a convention to simplify later steps
+        os.rename(extracted_dir, self.source_subfolder)
+
+        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.source_subfolder)
+
+        
     def build(self):
         cmake = CMake(self)
-        defs = {
-            'CMAKE_INSTALL_PREFIX': os.path.join(self.conanfile_directory, 'install'),
-            'CMAKE_POSITION_INDEPENDENT_CODE': self.options.fPIC,
-            'BUILD_SHARED_LIBS': self.options.shared
-        }
-        src = os.path.join(self.conanfile_directory, self.folder)
-        cmake.configure(build_dir='build', source_dir=src, defs=defs)
-        cmake.build(target='install')
-
+        cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        cmake.configure(build_folder=self.build_subfolder, source_folder=self.source_subfolder)
+        cmake.build()
+        cmake.install()
+        
     def package(self):
-        """ Define your conan structure: headers, libs and data. After building your
-            project, this method is called to create a defined structure:
-        """
-        folder = 'install'
-        self.copy(pattern="*.h", dst="include", src=folder, keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src=folder, keep_path=False)
-        self.copy(pattern="*.dll*", dst="bin", src=folder, keep_path=False)
-        self.copy(pattern="*.a", dst="lib", src=folder, keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src=folder, keep_path=False)
-        self.copy(pattern="*.dylib*", dst="lib", src=folder, keep_path=False)
+        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
+        # If so, you can replace all the steps below with the word "pass"
+        include_folder = os.path.join(self.source_subfolder, "include")
+        self.copy(pattern="LICENSE")
+        self.copy(pattern="*", dst="include", src=include_folder)
+        self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
+        
     def package_info(self):
-        self.cpp_info.libs = ["modplug"]
+        self.cpp_info.libs = tools.collect_libs(self)
